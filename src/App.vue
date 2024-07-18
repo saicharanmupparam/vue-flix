04b54b3c1af7c499085a6a3b045d2fad4bfc0922
Chen <saicharan.mupparam@eagles.oc.edu>
2024-07-18 21:03:41 +0000

    style adjustments; extract out main component; add routing;

--- src/App.vue
+++ src/App.vue
@@ -1,77 +1,16 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-import { ref, onMounted } from 'vue'
-import { supabase } from './lib/supabaseClient'
-import type { Movies } from './interfaces'
-
-const countries = ref([])
-const movies = ref<Movies[]>([])
-const items = [
-  {
-    src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
-  },
-  {
-    src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
-  },
-  {
-    src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
-  },
-  {
-    src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
-  },
-]
-
-async function getCountries() {
-  const { data: moviesRef } = await supabase.from('movies').select()
-  const { data: countriesRef } = await supabase.from('countries').select()
-  countries.value = countriesRef
-  movies.value = moviesRef
-
-  console.log(movies.value)
-}
-
-onMounted(() => {
-  // Load movies into state......
-
-  getCountries()
-})
-
-
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
   <header>
-    <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
-
-    <div class="wrapper">
-      <!-- <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav> -->
-      <ul v-for="movie in movies">
-        <img :src="movie.image" />
-        <br />
-        <img height="50px" width="100px" :src="movie.platform[0]" />
-        <br />
-        <li :key="movie.title">{{ movie.title }}</li>
-        <li :key="movie.languages">{{ movie.languages }}</li>
-        <li :key="movie.genre">{{ movie.genre }}</li>
-        <li :key="movie.category">{{ movie.category }}</li>
-        <li :key="movie.release_year">{{ movie.release_year }}</li>
-        <li :key="movie.recommendation">{{ movie.recommendation }}</li>
-      </ul>
-
-
-      <v-carousel hide-delimiters>
-        <v-carousel-item v-for="(item, i) in items" :key="i" :src="item.src" cover></v-carousel-item>
-      </v-carousel>
-    </div>
+    <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="25" height="25" /> -->
+    <div class="header"></div>
   </header>
-
   <RouterView />
+  <footer>
+    <div class="footer"></div>
+  </footer>
 </template>
 
 <style scoped>
@@ -83,6 +22,13 @@ header {
 .logo {
   display: block;
   margin: 0 auto 2rem;
+}
+
+header,
+footer {
+  height: 40px;
+  width: 100%;
+  background-color: rgb(120, 197, 223);
 }
 
 nav {
